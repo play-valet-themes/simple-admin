632db2bae3c5bf885721274873f6b215e5130a3f
KeigoShiraishi <nspfb656pro@yahoo.co.jp>
2017-04-22 23:00:30 +0900

    r

--- app/controllers/AdminController.scala
+++ app/controllers/AdminController.scala
@@ -8,12 +8,24 @@ import play.api.mvc._
 import scala.concurrent.ExecutionContext
 
 @Singleton
-class AdminController @Inject()(
-                                 val messagesApi: MessagesApi)(implicit executor: ExecutionContext)
+class AdminController @Inject()(val messagesApi: MessagesApi)(implicit executor: ExecutionContext)
   extends Controller with I18nSupport {
 
   def index = Action { implicit request =>
-    Ok(views.html.admin.posts.list("Your new application is ready."))
+    Ok(views.html.admin.crud_template.list("Your new application is ready."))
   }
 
+  def showCreate = Action { implicit request =>
+    Ok(views.html.admin.crud_template.store("Your new application is ready."))
+  }
+
+  def showEdit = Action { implicit request =>
+    Ok(views.html.admin.crud_template.edit("Your new application is ready."))
+  }
+
+  def showDetail = Action { implicit request =>
+    Ok(views.html.admin.crud_template.detail("Your new application is ready."))
+  }
 }
+
+
